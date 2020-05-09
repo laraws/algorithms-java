5393a2a928916bebd40ff8844995a6702a928c0f
hasee-linux <larawsw@outlook.com>
2020-05-09 16:04:07 +0800

    feat: update selection sort input method

--- src/com/laraws/java/algs/Selection.java
+++ src/com/laraws/java/algs/Selection.java
@@ -1,24 +1,34 @@
 package com.laraws.java.algs;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Selection {
-    public static void main(String[] args) {
-        int[] a = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 11, 23, 33, 88};
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File(args[0]));
+        ArrayList<Integer> list = new ArrayList<>();
+        int i = 0;
+        while(scanner.hasNextInt()){
+            list.add(scanner.nextInt());
+        }
+        int[] a = list.stream().mapToInt(Integer::intValue).toArray();
         compare(a);
         System.out.println(Arrays.toString(a));
     }
 
-    public static void compare(int[] a) {
-        int len = a.length;
+    public static void compare(int[] b) {
+        int len = b.length;
         for (int i = 0; i < len; i++) {
             int min = i;
             for (int j = i + 1; j < len; j++) {
-                if (a[min] > a[j]) {
+                if (b[min] > b[j]) {
                     min = j;
                 }
             }
-            swap(a, i, min);
+            swap(b, i, min);
         }
     }
 
